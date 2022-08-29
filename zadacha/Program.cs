4569b894ba9da07b427987e681a34587889a1484
Marta <[martasunnyy@gmail.com]>
2022-08-29 21:48:51 +0300

    решила доп задачу

---
+++ zadacha/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23. 
+
+Console.Write("Введи цифру: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void DividedByTwoNumbers (int number)
+{
+  if (number % 7 == 0 && number % 23 == 0) 
+  {
+     Console.WriteLine("это  число кратное 7 и 23");
+  }
+ 
+  else Console.WriteLine("это не кратное число");
+}
+
+DividedByTwoNumbers(number);
+
--- задача_15/Program.cs
+++ задача_15/Program.cs
@@ -3,11 +3,14 @@
 Console.Write("Введи цифру, обозначающую день недели: ");
 int dayNumber = Convert.ToInt32(Console.ReadLine());
 
-void CheckingTheDayOfTheWeek (int dayNumber) {
-  if (dayNumber == 6 || dayNumber == 7) {
-  Console.WriteLine("это  выходной");
+void CheckingTheDayOfTheWeek (int dayNumber)
+{
+  if (dayNumber == 6 || dayNumber == 7) 
+  {
+     Console.WriteLine("это  выходной");
   }
-  else if (dayNumber < 1 || dayNumber > 7) {
+  else if (dayNumber < 1 || dayNumber > 7) 
+  {
     Console.WriteLine("это не день недели");
   }
   else Console.WriteLine("это не выходной день");
